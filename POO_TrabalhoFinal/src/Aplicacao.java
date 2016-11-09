f86c0181e35b3c17112c3e34f08e1b24f797a595
jmfantin <15104312@W3070497.portoalegre.pucrsnet.br>
2016-11-09 13:55:48 -0200

    Loop de execução de ciclos na Aplicacao

--- POO_TrabalhoFinal/src/Aplicacao.java
+++ POO_TrabalhoFinal/src/Aplicacao.java
@@ -1,9 +1,29 @@
-
+import java.util.Scanner;
 public class Aplicacao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
 
+
+        System.out.println("\nEsta aplicação funciona por ciclos de execução. Digite 0 para sair, e qualquer outra tecla para iniciar/continuar.");
+
+        System.out.println("\n>SIMULAÇÃO INICIADA<\n");
+        
+        int cont = 0;
+		while(waitPress() != '0'){
+			
+			
+			
+
+			System.out.println(cont);
+			cont++;
+		
+		}//fecha while
+	}//fecha main
+	
+	public static char waitPress(){ //eu nem preciso do retorno, mas dava erro de execução se fizesse void...
+		Scanner wp = new Scanner (System.in);
+		return wp.next().charAt(0);
 	}
 
 }
